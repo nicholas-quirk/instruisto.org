fcd3a9f869b051d5871c12a3ef1f0df16a9ae3c7
unknown <nmquirk@gmail.com>
2013-03-24 16:09:59 -0400

    Minimum lein version.

--- project.clj
+++ project.clj
@@ -7,6 +7,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
+  :min-lein-version "2.0.0"
+
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [domina "1.0.0"]
                  [compojure "1.1.0"]
